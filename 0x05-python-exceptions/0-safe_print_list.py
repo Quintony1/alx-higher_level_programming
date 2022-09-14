6739dcf23e85e8925408dec07aeb32902b407a38
Quintony1 <nyashajerahuni@gmail.com>
2022-09-13 20:57:42 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,9 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end='')
             counter += 1
-        except:
-            break
-        i += 1
+            except:
+                break
+            i += 1
     print()
 
     return counter
